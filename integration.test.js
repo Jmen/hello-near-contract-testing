52701f42d45b8d23b9891bcd33b0e47942787fd8
Jaimen Lathia <jaimen@endlesss.fm>
2022-02-02 11:54:57 +0000

    contract call

--- integration.test.js
+++ integration.test.js
@@ -1,24 +1,34 @@
-const { connect, keyStores, utils } = require("near-api-js");
-const keyStore = new keyStores.InMemoryKeyStore();
-const config = {
-    keyStore,
-    networkId: "testnet",
-    nodeUrl: "https://rpc.testnet.near.org",
-};
+const { connect, keyStores, utils, Contract } = require("near-api-js");
+
+require('dotenv').config()
+
+const networkId = "testnet";
 
 it('call a near nft contract', async() => {
 
-    const keyPair = new utils.key_pair.KeyPairEd25519('private-key');
+    const keyPair = new utils.key_pair.KeyPairEd25519(process.env.TEST_ACCOUNT_PRIVATE_KEY);
 
-    await keyStore.setKey(config.networkId, "j_men.testnet", keyPair);
+    const keyStore = new keyStores.InMemoryKeyStore();
+    await keyStore.setKey(networkId, "j_men.testnet", keyPair);
 
-    const near = await connect(config);
+    const near = await connect({
+        keyStore,
+        networkId,
+        nodeUrl: "https://rpc.testnet.near.org",
+    });
+
     const account = await near.account("j_men.testnet");
 
-    const metadataResult = await account.functionCall({
-        contractId: "nft-example.j_men.testnet",
-        methodName: "nft_metadata"
-    })
+    const contract = new Contract(
+        account,
+        'nft-example.j_men.testnet',
+        {
+            viewMethods: ['nft_metadata'],
+            changeMethods: []
+        }
+    );
 
-    expect(metadataResult).not.toBeUndefined();
+    const result = await contract.nft_metadata();
+
+    expect(result.spec).toBe("nft-1.0.0");
 });
